726e495b4e48899b96afd6dcf67541fb6bc757e1
pdchi <pdchi93@gmail.com>
2021-01-26 02:12:47 -0500

    Update gaussian_fitter_scipy15.py
    adding additional spacing

--- GaussianFitter/lib/gaussian_fitter_scipy15.py
+++ GaussianFitter/lib/gaussian_fitter_scipy15.py
@@ -17,7 +17,7 @@ def gaussfit(x, y, double=False, graph=False, quiet=False):
 	Returns:
 	6 element 1d array for double
 	5 element 1d array for single
-    see text output for the definitions of returned values
+	see text output for the definitions of returned values
 	"""
 
 	# Set iteration number for curve_fit
@@ -28,6 +28,7 @@ def gaussfit(x, y, double=False, graph=False, quiet=False):
 	FWHM1 = np.sqrt(np.abs(np.sum((x-peak1)**2.*y)/np.sum(y)))
 	sig_noise = np.std(y)
 	bkg_counts = np.mean(y)
+	
 	# Decide if peak is positive or negative w.r.t. continuum
 	if np.abs(bkg_counts-np.max(y)) > np.abs(bkg_counts-np.min(y)):
 		intensity1 = np.max(y)-bkg_counts
@@ -42,19 +43,24 @@ def gaussfit(x, y, double=False, graph=False, quiet=False):
 		def double_gaussian_fit(x, I, lambda_c, fwhm, R, b):
 			I = np.abs(I)
 			return  I*np.exp(-(x-lambda_c)**2.0/(2.0*(fwhm / (2.0 * np.sqrt(2.0 * np.log(2.0))))**2.0))+I*np.exp(-(x-lambda_c-R)** 2.0/(2.0*(fwhm / (2.0 * np.sqrt(2.0 * np.log(2.0))))**2.0))+b
+		
 		# Set parameter bounds and run optimization
 		#parambounds=([0.,peak1-200,.1,-30,0.],[np.inf,peak1+200,np.inf,30,np.inf])
 		popt_double,pcov_double = curve_fit(double_gaussian_fit, x, y, p0=[intensity1, peak1-peaksep, FWHM1*.5, peaksep, bkg_counts])
 		popt_double[0]=np.abs(popt_double[0])
+		
 		# Double Gaussian Chi2
 		chi2_double = sum(((y - double_gaussian_fit(x,popt_double[0],popt_double[1],popt_double[2],popt_double[3],popt_double[4]))**2)/ sig_noise**2)  / (len(y) - 5)
+		
 		# Equivalent width
 		fit = lambda t : popt_double[0]*np.exp(-(t-popt_double[1])**2./(2.*(popt_double[2]/2.355)**2.)) + popt_double[0]*np.exp(-(t-popt_double[1]-popt_double[3])**2./(2.*(popt_double[2]/2.355)**2.))
 		eqv = np.sum(-fit(x)) * (x[1] - x[0]) / popt_double[4]
+		
 		# Optional text output
 		if not quiet:
 			print('Fit of double Gaussian, with chi2 of '+str(chi2_double))
 			print('Outputs: Peak1 intensity, Peak1 location, FWHM, Relative Sep, Continuum, Equivalent width')
+		
 		# Optional graphical output
 		if graph:
 			plt.clf()
